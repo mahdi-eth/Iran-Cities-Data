12e6efff5ade5b899eb9817c546e3d952a02b4d1
Mahdi Ettehadnejad <mehdi@inja.app>
2023-12-29 16:44:13 +0330

    feat(data): add files

---
+++ SQL/provinces.sql
@@ -0,0 +1,35 @@
+CREATE TABLE mytable(
+   id   INTEGER  NOT NULL PRIMARY KEY 
+  ,name VARCHAR(19) NOT NULL
+);
+INSERT INTO mytable(id,name) VALUES (1,'آذربایجان شرقی');
+INSERT INTO mytable(id,name) VALUES (2,'آذربایجان غربی');
+INSERT INTO mytable(id,name) VALUES (3,'اردبیل');
+INSERT INTO mytable(id,name) VALUES (4,'اصفهان');
+INSERT INTO mytable(id,name) VALUES (5,'ایلام');
+INSERT INTO mytable(id,name) VALUES (6,'بوشهر');
+INSERT INTO mytable(id,name) VALUES (7,'تهران');
+INSERT INTO mytable(id,name) VALUES (8,'چهارمحال و بختیاری');
+INSERT INTO mytable(id,name) VALUES (9,'خراسان جنوبی');
+INSERT INTO mytable(id,name) VALUES (10,'خراسان رضوی');
+INSERT INTO mytable(id,name) VALUES (11,'خراسان شمالی');
+INSERT INTO mytable(id,name) VALUES (12,'خوزستان');
+INSERT INTO mytable(id,name) VALUES (13,'زنجان');
+INSERT INTO mytable(id,name) VALUES (14,'سمنان');
+INSERT INTO mytable(id,name) VALUES (15,'سیستان و بلوچستان');
+INSERT INTO mytable(id,name) VALUES (16,'فارس');
+INSERT INTO mytable(id,name) VALUES (17,'قزوین');
+INSERT INTO mytable(id,name) VALUES (18,'قم');
+INSERT INTO mytable(id,name) VALUES (19,'البرز');
+INSERT INTO mytable(id,name) VALUES (20,'کردستان');
+INSERT INTO mytable(id,name) VALUES (21,'کرمان');
+INSERT INTO mytable(id,name) VALUES (22,'کرمانشاه');
+INSERT INTO mytable(id,name) VALUES (23,'کهگیلویه و بویراحمد');
+INSERT INTO mytable(id,name) VALUES (24,'گلستان');
+INSERT INTO mytable(id,name) VALUES (25,'لرستان');
+INSERT INTO mytable(id,name) VALUES (26,'گیلان');
+INSERT INTO mytable(id,name) VALUES (27,'مازندران');
+INSERT INTO mytable(id,name) VALUES (28,'مرکزی');
+INSERT INTO mytable(id,name) VALUES (29,'هرمزگان');
+INSERT INTO mytable(id,name) VALUES (30,'همدان');
+INSERT INTO mytable(id,name) VALUES (31,'یزد');
